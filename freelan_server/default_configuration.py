894d691d50a3efe3695e6f13e0caea7072428db9
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-06-28 20:49:18 +0200

    Added comments to the default configuration

--- freelan_server/default_configuration.py
+++ freelan_server/default_configuration.py
@@ -5,10 +5,26 @@ The default configuration.
 import os
 import tempfile
 
+# The host to listen on.
 HOST = '::'
+
+# Whether to enable debug mode.
 DEBUG = False
+
+# The database URI.
+#
+# For real installations, you probably want to change the target directory to a
+# more permanent location or to change the database provider to something more
+# production-suited.
 SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % os.path.join(
     tempfile.gettempdir(),
     'freelan_server.db'
 )
+
+# The secret key used for session securing.
+#
+# This key *MUST* be changed and kept secret !
+#
+# Failing to do so would allow anyone to steal sessions and you don't want
+# that, do you ?
 SECRET_KEY = 'please change this secret key'
