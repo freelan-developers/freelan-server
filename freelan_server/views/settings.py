77e83486dcb4540f56eeedf79ff6d8d569ea999c
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-06-29 19:23:38 +0200

    Added basic certificate checking

--- freelan_server/views/settings.py
+++ freelan_server/views/settings.py
@@ -8,6 +8,8 @@ from flask import redirect, url_for, request, session, render_template
 from flask_login import current_user, login_required
 from freelan_server.database import DATABASE, Setting
 
+import M2Crypto as m2
+
 class SettingsView(MethodView):
     """
     The settings view.
@@ -33,16 +35,24 @@ class SettingsView(MethodView):
 
     def post(self):
 
-        for key in self.known_keys:
-            if key in request.values:
-                setting = Setting.query.get(key)
+        authority_certificate_error = None
+        authority_certificate = request.values['authority_certificate']
 
-                if setting:
-                    setting.value = request.values[key]
-                else:
-                    setting = Setting(key, request.values[key])
+        if authority_certificate:
+            try:
+                cert = m2.X509.load_cert_string(authority_certificate, m2.X509.FORMAT_PEM)
+            except m2.X509.X509Error, ex:
+                authority_certificate_error = 'Invalid certificate: %s' % ex
 
-                DATABASE.session.add(setting)
-                DATABASE.session.commit()
 
-        return self.get()
+        all_settings = Setting.query.all()
+
+        settings = dict((s.key, s.value) for s in all_settings if s.key in self.known_keys)
+        unknown_settings = dict((s.key, s.value) for s in all_settings if s.key not in self.known_keys)
+
+        return render_template(
+            'pages/settings.html',
+            unknown_settings=unknown_settings,
+            authority_certificate_error=authority_certificate_error,
+            **settings
+        )
--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         'Flask-Login',
         'Flask-KVSession',
         'Flask-Gravatar',
+        'M2Crypto',
     ],
     entry_points = {
         'console_scripts': [
