d4ffa1471037e8caf213a595e2879c0f4be51185
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-06-30 10:04:31 +0200

    Fixed settings update

--- freelan_server/database/__init__.py
+++ freelan_server/database/__init__.py
@@ -30,6 +30,44 @@ class Setting(DATABASE.Model):
         self.value = value
         self.creation_date = datetime.datetime.now()
 
+    @staticmethod
+    def get(key):
+        """
+        Get a setting with the specified key or create one if none exists.
+        """
+
+        setting = Setting.query.get(key)
+
+        if not setting:
+            setting = Setting(key)
+
+        return setting
+
+    @staticmethod
+    def get_value(key, default=None):
+        """
+        Get a setting's value.
+        """
+
+        setting = Setting.query.get(key)
+
+        if not setting:
+            return default
+
+        return setting.value
+
+    @staticmethod
+    def set_value(key, value):
+        """
+        Set a setting.
+
+        The setting change is added to the current database session, but it is still up to you to commit that session.
+        """
+
+        setting = Setting.get(key)
+        setting.value = value
+        DATABASE.session.add(setting)
+
 NetworkUserTable = DATABASE.Table(
     'network_user',
     DATABASE.Column('network_id', DATABASE.Integer, DATABASE.ForeignKey('network.id')),
--- freelan_server/views/settings.py
+++ freelan_server/views/settings.py
@@ -17,42 +17,50 @@ class SettingsView(MethodView):
 
     decorators = [login_required]
 
-    def __init__(self):
-
-        self.known_keys = [
-            'authority_certificate',
-            'authority_private_key',
-        ]
-
     def get(self):
+        """
+        Get the settings page.
+        """
 
-        all_settings = Setting.query.all()
-
-        settings = dict((s.key, s.value) for s in all_settings if s.key in self.known_keys)
-        unknown_settings = dict((s.key, s.value) for s in all_settings if s.key not in self.known_keys)
-
-        return render_template('pages/settings.html', unknown_settings=unknown_settings, **settings)
-
-    def post(self):
-
-        authority_certificate_error = None
-        authority_certificate = request.values['authority_certificate']
-
-        if authority_certificate:
-            try:
-                cert = m2.X509.load_cert_string(authority_certificate, m2.X509.FORMAT_PEM)
-            except m2.X509.X509Error, ex:
-                authority_certificate_error = 'Invalid certificate: %s' % ex
-
-
-        all_settings = Setting.query.all()
-
-        settings = dict((s.key, s.value) for s in all_settings if s.key in self.known_keys)
-        unknown_settings = dict((s.key, s.value) for s in all_settings if s.key not in self.known_keys)
+        authority_certificate = Setting.get_value('authority_certificate')
+        authority_private_key = Setting.get_value('authority_private_key')
 
         return render_template(
             'pages/settings.html',
-            unknown_settings=unknown_settings,
+            authority_certificate=authority_certificate,
+            authority_private_key=authority_private_key,
+        )
+
+    def post(self):
+        """
+        Update the settings.
+        """
+
+        authority_certificate_error = None
+        authority_certificate = request.values['authority_certificate']
+        authority_private_key_error = None
+        authority_private_key = request.values['authority_private_key']
+
+        if authority_certificate:
+            try:
+                cert = m2.X509.load_cert_string(str(authority_certificate), m2.X509.FORMAT_PEM)
+                Setting.set_value('authority_certificate', authority_certificate)
+            except m2.X509.X509Error, ex:
+                authority_certificate_error = 'Invalid certificate: %s' % ex
+
+        if authority_private_key:
+            try:
+                cert = m2.RSA.load_key_string(str(authority_private_key))
+                Setting.set_value('authority_private_key', authority_private_key)
+            except m2.RSA.RSAError, ex:
+                authority_private_key_error = 'Invalid private key: %s' % ex
+
+        DATABASE.session.commit()
+
+        return render_template(
+            'pages/settings.html',
             authority_certificate_error=authority_certificate_error,
-            **settings
+            authority_certificate=authority_certificate,
+            authority_private_key_error=authority_private_key_error,
+            authority_private_key=authority_private_key,
         )
