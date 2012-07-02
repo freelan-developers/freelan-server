190e230b41f7a25bd8ab4e5f7abb071e8c4e6060
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-07-02 21:31:32 +0200

    Added a way to remove the certificate and private key from the settings

--- freelan_server/views/settings.py
+++ freelan_server/views/settings.py
@@ -37,26 +37,30 @@ class SettingsView(MethodView):
         """
 
         authority_certificate_error = None
-        authority_certificate = str(request.values['authority_certificate'])
+        authority_certificate = request.values['authority_certificate'] or ''
         authority_private_key_error = None
-        authority_private_key = str(request.values['authority_private_key'])
+        authority_private_key = request.values['authority_private_key'] or ''
         authority_private_key_passphrase = str(request.values['authority_private_key_passphrase'] or '')
 
         if authority_certificate:
             try:
-                cert = m2.X509.load_cert_string(authority_certificate, m2.X509.FORMAT_PEM)
+                cert = m2.X509.load_cert_string(str(authority_certificate), m2.X509.FORMAT_PEM)
                 authority_certificate = cert.as_pem()
                 Setting.set_value('authority_certificate', authority_certificate)
             except m2.X509.X509Error, ex:
                 authority_certificate_error = 'Certificate error: %s' % ex
+        else:
+            Setting.set_value('authority_certificate', '')
 
         if authority_private_key:
             try:
-                key = m2.RSA.load_key_string(authority_private_key, callback=(lambda v: authority_private_key_passphrase))
+                key = m2.RSA.load_key_string(str(authority_private_key), callback=(lambda v: authority_private_key_passphrase))
                 authority_private_key = key.as_pem(cipher=None)
                 Setting.set_value('authority_private_key', authority_private_key)
             except m2.RSA.RSAError, ex:
                 authority_private_key_error = 'Private key error: %s' % ex
+        else:
+            Setting.set_value('authority_private_key', '')
 
         DATABASE.session.commit()
 
