a2c3f817cc1e530792c10efc98582c44b5436be5
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-07-07 13:52:10 +0200

    Better settings wizard implementation

--- freelan_server/views/settings_wizard.py
+++ freelan_server/views/settings_wizard.py
@@ -4,26 +4,35 @@ The settings wizard view.
 
 from flask.views import MethodView
 
-from flask import redirect, url_for, request, session, render_template
+from flask import redirect, url_for, request, session, render_template, abort
 from flask_login import current_user, login_required
 from freelan_server.database import DATABASE, Setting
 
 import M2Crypto as m2
 
-class SettingsWizardView(MethodView):
+@login_required
+def settings_wizard(step=None):
     """
     The settings wizardview.
     """
 
-    decorators = [login_required]
+    min_step=1
+    max_step=3
 
-    def get(self):
-        """
-        Get the settings wizard page.
-        """
+    # Redirect if step is not defined
+    if step is None:
+        return redirect(url_for('settings_wizard', step=min_step))
 
-        return render_template(
-            'pages/settings_wizard.html',
-        )
+    # Abort if step is invalid
+    if (step < min_step) or (step > max_step):
+        abort(404)
 
-    post = get
+    authority_private_key = Setting.get_value('authority_private_key')
+
+    return render_template(
+        'pages/settings_wizard_%s.html' % step,
+        step=step,
+        min_step=min_step,
+        max_step=max_step,
+        authority_private_key=authority_private_key,
+    )
