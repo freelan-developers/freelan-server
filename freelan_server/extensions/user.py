05cc46c1d78d178e8a8d2f32433cda427595d3c6
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-07-22 12:30:36 +0200

    Made render_user_list method return markup text

--- freelan_server/extensions/user.py
+++ freelan_server/extensions/user.py
@@ -4,12 +4,14 @@ The user filters.
 
 from flask import render_template
 
+from jinja2 import Markup
+
 def render_user_list(list_name, users):
     """
     Render a user list.
     """
 
-    return render_template('elements/user_list.html', list_name=list_name, users=users)
+    return Markup(render_template('elements/user_list.html', list_name=list_name, users=users))
 
 def register_user_functions(app):
     """
