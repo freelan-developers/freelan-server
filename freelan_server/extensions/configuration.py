a7210673ee81f5c87fa9b8aa53a59d7987185dd4
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-09-30 11:04:18 +0200

    Added a hack to allow loading of trusted certificates

--- freelan_server/extensions/configuration.py
+++ freelan_server/extensions/configuration.py
@@ -14,8 +14,12 @@ def register_configuration_information(app):
     authority_private_key_passphrase = app.config['AUTHORITY_PRIVATE_KEY_PASSPHRASE'] or ''
 
     try:
-        authority_certificate = m2.X509.load_cert(
-            authority_certificate_file,
+        # Below is a hack that allow loading of trusted certificates
+        # I don't like it but it seems M2Crypto doesn't provide a way for
+        # loading those, so we don't really have a choice.
+
+        authority_certificate = m2.X509.load_cert_string(
+            open(authority_certificate_file, 'r').read().replace(' TRUSTED', ''),
             m2.X509.FORMAT_PEM
         )
         authority_certificate_error = None
