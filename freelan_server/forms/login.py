a263d95a5efffe2fdab12becfbd5388975428734
Don Spaulding <donspauldingii@gmail.com>
2014-09-24 16:55:28 -0500

    Update login.py

--- freelan_server/forms/login.py
+++ freelan_server/forms/login.py
@@ -10,7 +10,7 @@ class LoginForm(Form):
     """
     The login form.
     """
-    username = TextField('Username', [validators.Required(), validators.Length(min=1, max=80)])
-    password = PasswordField('Password', [validators.Required()])
+    username = TextField('Username', [validators.InputRequired(), validators.Length(min=1, max=80)])
+    password = PasswordField('Password', [validators.InputRequired()])
     recaptcha = RecaptchaField()
     remember_me = BooleanField('Remember me')
